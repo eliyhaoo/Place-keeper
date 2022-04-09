68ca557a88fdca9cd4234271fb4864be8073571d
Eli T <eliyhaoo@gmail.com>
2022-04-09 17:53:35 +0300

    sec update

--- js/controller.js
+++ js/controller.js
@@ -13,3 +13,43 @@ function showAge(age){
  document.querySelector('.age-display').innerText = age
  
 }
+
+
+function getPosition(){
+    console.log('getting pos');
+    navigator.geolocation.getCurrentPosition(getLocation,handleLocationErorr)
+
+}
+
+function getLocation(pos){
+    initMap(pos.coords.latitude,pos.coords.longitude)
+     console.log(pos.coords.latitude,pos.coords.longitude)
+     console.log('getting location',pos)
+}
+
+function handleLocationErorr(){
+    console.log('handling erorr');
+}
+
+function initMap(lat,lng) {
+    const elMap = document.querySelector('.map-container')
+    let options = {
+        center: {lat,lng},
+        zoom: 10
+    }
+
+    const map = new google.maps.Map(elMap,options)
+}
+
+function mapReady(){
+    console.log('map is ready');
+}
+
+
+// function initMap(lat,lng){
+//     console.log('lat',lat);
+//     console.log('lng',lng);
+
+// }
+
+
